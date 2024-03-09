7b13c16740511143ff87a685b62aa405774cf829
Tokio Idzaki <morphinethings@gmail.com>
2024-03-09 13:31:50 +0300

    add themes, ModeToggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import {Button} from "@/components/ui/button";
+import {Button} from "@/shared/ui/button";
+import {ModeToggle} from "@/shared/ui/ModeToggle";
 
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
-            <Button variant="outline">Button</Button>
+            <Button
+                className='Button'
+                variant="outline">Button</Button>
+
+            <ModeToggle/>
 
         </main>
     );
